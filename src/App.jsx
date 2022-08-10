4f5861ff990db03e889e034e7a8d60388374a427
singhhimansh <him.signum802@gmail.com>
2022-08-10 16:28:39 +0530

    init: sgs react site🚀

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App bg-red-200 m-20 animate__animated  animate__bounce">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, suscipit delectus molestiae dolor, aperiam accusamus ratione veniam nobis sapiente ad fugit voluptate rerum culpa quas. Autem nisi tempore minima cum voluptatibus, maxime distinctio vitae illo provident aspernatur quia explicabo, dolore possimus consequatur dicta! Voluptatum expedita quam, fugiat libero mollitia possimus.
+    </div>
+  );
+}
+
+export default App;
